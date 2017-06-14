160f3c4def639a08e80ec6e6cf716d2f1d7ea9d0
elcoruco <el.coruco@gmail.com>
2017-06-14 10:50:33 -0500

    add server response

--- app/Http/Controllers/OpaApi.php
+++ app/Http/Controllers/OpaApi.php
@@ -4,9 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Opa;
+
 class OpaApi extends Controller
 {
-  public function index(){
-    echo ":D";
+  public function index($id){
+    $res = Opa::where("cve_ppi", $id)->get();
+
+    foreach($res as $r){
+      $r->urlimages = [];
+      $r->monto_asigautact = 0;
+      $r->desc_clas_ciudadana = $r->clasificacion_ciudadana;
+      $r->fase = "concluido";
+    }
+    $response = [];
+    $response["total"]      = $res->count();
+    $response["resultados"] = $res;
+    return response()->json($response)->header("Access-Control-Allow-Origin", "*");
   }
 }
